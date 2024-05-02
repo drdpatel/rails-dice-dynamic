b3216604ce1af6a7d4f1f5e0b63eebc5ee217121
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-05-02 01:47:48 +0000

    RC done

--- app/controllers/dice_roll_controller.rb
+++ app/controllers/dice_roll_controller.rb
@@ -1,4 +1,27 @@
 class DiceRollController < ApplicationController
 
-render ({ :template => "path/home_page" })
+def home
+render ({ :template => "path/home" })
+end
+
+def twosixdice
+render ({ :template => "path/2d6" })
+end
+
+
+def twotendice
+  render ({ :template => "path/2d10" })
+
+end
+
+def onetwentydice
+  render ({ :template => "path/1d20" })
+end
+
+
+def fivefourdice
+  render ({ :template => "path/5d4" })
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
-get("/", { :controller => "dice_roll", :action => "home_page" })
+get("/", { :controller => "dice_roll", :action => "home" })
+get("dice/2/6", { :controller => "dice_roll", :action => "twosixdice" })
+get("dice/2/10", { :controller => "dice_roll", :action => "twosixdice" })
+get("dice/1/20", { :controller => "dice_roll", :action => "twosixdice" })
+get("dice/5/4", { :controller => "dice_roll", :action => "twosixdice" })
+
 end

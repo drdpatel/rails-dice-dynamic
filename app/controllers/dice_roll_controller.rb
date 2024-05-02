f9b46c42faa7a6cd04b4a0511ca0b30faaefdeba
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-05-02 02:03:13 +0000

    error

--- app/controllers/dice_roll_controller.rb
+++ app/controllers/dice_roll_controller.rb
@@ -1,25 +1,45 @@
 class DiceRollController < ApplicationController
 
 def home
-render ({ :template => "path/home" })
+ 
+
+  render({ :template => "path/home" })
 end
 
 def twosixdice
-render ({ :template => "path/2d6" })
+
+  @first_die = rand(1..6)
+  @second_die = rand(1..6)
+
+  render({ :template => "path/twosix" })
 end
 
 
 def twotendice
+
+  @first_die = rand(1..10)
+  @second_die = rand(1..10)
+
+
   render ({ :template => "path/2d10" })
 
 end
 
 def onetwentydice
+
+  @first_die = rand(1..20)
+
   render ({ :template => "path/1d20" })
 end
 
 
 def fivefourdice
+
+  @first_die = rand(1..4)
+  @second_die = rand(1..4)
+  @third_die = rand(1..4)
+  @forth_die = rand(1..4)
+
   render ({ :template => "path/5d4" })
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
 get("/", { :controller => "dice_roll", :action => "home" })
 get("dice/2/6", { :controller => "dice_roll", :action => "twosixdice" })
-get("dice/2/10", { :controller => "dice_roll", :action => "twosixdice" })
-get("dice/1/20", { :controller => "dice_roll", :action => "twosixdice" })
-get("dice/5/4", { :controller => "dice_roll", :action => "twosixdice" })
+get("dice/2/10", { :controller => "dice_roll", :action => "twosixten" })
+get("dice/1/20", { :controller => "dice_roll", :action => "twoonetwenty" })
+get("dice/5/4", { :controller => "dice_roll", :action => "twofivefour" })
 
 end
